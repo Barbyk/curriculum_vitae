c5377ff8a9aca4209fc6c9b67ece27942bf30814
Barbara <bkpadet@gmail.com>
2019-03-17 01:00:42 +0100

    Updates

---
+++ precache-manifest.7bcb19777c40da965af9ea9685af660e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e683589b70e9372ef3481d7d84ed32e",
+    "url": "/curriculum_vitae/static/media/hexagon.5e683589.png"
+  },
+  {
+    "revision": "6ec5c0b748d83b42f40a",
+    "url": "/curriculum_vitae/static/js/runtime~main.d91bf91e.js"
+  },
+  {
+    "revision": "66b31e0457cb8e217666",
+    "url": "/curriculum_vitae/static/js/main.2cf53366.chunk.js"
+  },
+  {
+    "revision": "9648e7906c14fa53ce37",
+    "url": "/curriculum_vitae/static/js/2.e8bb9b45.chunk.js"
+  },
+  {
+    "revision": "66b31e0457cb8e217666",
+    "url": "/curriculum_vitae/static/css/main.d5584599.chunk.css"
+  },
+  {
+    "revision": "9648e7906c14fa53ce37",
+    "url": "/curriculum_vitae/static/css/2.6f09b03c.chunk.css"
+  },
+  {
+    "revision": "a937cf5bbacaa07033aa5c75914c5f9e",
+    "url": "/curriculum_vitae/index.html"
+  }
+];
